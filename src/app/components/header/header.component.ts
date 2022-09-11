20876bb4c3898b3cf2c862966da609d438251fd5
rubenpena64 <109831018+rubenpena64@users.noreply.github.com>
2022-09-11 11:50:47 -0300

    Header creado

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  title:string = 'Task List';
   constructor() { }
+
 
   ngOnInit(): void {
   }
